3335d7a1b71745547304954e4fb6a8efcd58bba5
Mridul Hasan <125135774+mridul360@users.noreply.github.com>
2026-04-21 04:30:13 +0600

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-
-
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
